7f1507773d205f4a61e53f8d5659bd3e01677021
khoanetroi <148301883+khoanetroi@users.noreply.github.com>
2023-10-19 23:34:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'PhuongPhuong';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người khoa iuu';
+const contentCard = 'Chúc PhuongPhuong 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với chị trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'as5.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
